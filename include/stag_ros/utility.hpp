a106d5e8ece2106e129ba1cb8f7971226ebf7282
Johannes Meyer <johannes@intermodalics.eu>
2024-02-23 12:43:26 +0100

    stag_ros: support BGRA8 image encoding

--- include/stag_ros/utility.hpp
+++ include/stag_ros/utility.hpp
@@ -3,13 +3,14 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/image_encodings.hpp>
 
 namespace stag_ros {
 
 // Read image from msg and convert it to grayscale, checks provided for rgb8
 // and bgr8, default to mono8
 inline bool msgToGray(const sensor_msgs::msg::Image::ConstSharedPtr &msg, cv::Mat &gray) {
-  if (msg->encoding.compare("bgr8") == 0) {
+  if (msg->encoding.compare("bgr8") == 0 || msg->encoding == sensor_msgs::image_encodings::BGRA8) {
     cv::Mat src = cv_bridge::toCvShare(msg, msg->encoding)->image;
     cv::cvtColor(src, gray, cv::COLOR_BGRA2GRAY);
     return true;
